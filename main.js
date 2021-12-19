45f6b16bab629defabd0d2902b83b6536c65644c
Pietrocaruso <48248457+Pietrocaruso@users.noreply.github.com>
2021-12-19 11:47:45 +0100

    index

---
+++ main.js
@@ -0,0 +1,8 @@
+var preloader = document.getElementById("container-preloader")
+
+
+preloader.style.width = window.innerWidth / 4 + "px";
+preloader.style.height = window.innerHeight / 4 + "px"; 
+console.log(window.innerHeight,window.innerWidth)
+preloader.style.marginTop = window.innerHeight / 2  +  "px"
+
