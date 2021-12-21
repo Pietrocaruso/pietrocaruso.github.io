dbfa22d49c9b3e92cbf27a557d65697d515bc5b7
Pietrocaruso <48248457+Pietrocaruso@users.noreply.github.com>
2021-12-21 14:20:29 +0100

    some change

--- elisa/regalo/main.js
+++ elisa/regalo/main.js
@@ -4,11 +4,19 @@ var glitch = document.getElementById("glitch-js")
 glitch.style.display = "block";
 
 glitch.style.marginTop = window.innerHeight / 2 + "px"
+var img = document.getElementById("img")
 
 function ciao (){ 
    console.log(window.location.pathname)
     console.log(window.location.pathname.replace("index.html", "scusa.html"))
     window.location.href = window.location.href.replace("index.html", "scusa.html")
 }
-
-setTimeout(ciao,1500)
+if (window.innerHeight > window.innerWidth){
+    img.src = "layout-vertical"
+    img.style.width = window.innerHeight
+    img.style.height = window.innerWidth
+}else{
+    img.style.width = window.innerWidth
+    img.style.height = window.innerHeight
+}
+setTimeout(ciao,2000)
