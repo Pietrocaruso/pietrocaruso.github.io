77cc628b802df078dd333578ef580f461ee5f1c5
Pietrocaruso <48248457+Pietrocaruso@users.noreply.github.com>
2021-12-24 12:59:13 +0100

    start with animation

--- eli/main.js
+++ eli/main.js
@@ -1,24 +1,26 @@
 var landscape = document.getElementById("landscape"); 
-var nome = document.getElementById("name")
-var presenta = document.getElementById("presenta")
-var presentation = document.getElementById("presentatizione")
+var lettera = document.getElementById("lettera")
 
 portrait : Boolean
+
 if(window.innerHeight > window.innerWidth){
-    nome.hidden = true;
-    presenta.hidden = true;
+    
     landscape.hidden =false;
-    alert(window.innerHeight.toString() + " "+  window.innerWidth.toString())
+    lettera.hidden = true;
     document.body.style.backgroundColor = "#247ba0"
 }else {
+    lettera.hidden = false
     landscape.style.display = "none";
-    presentation.hidden = false; 
-    presentation.style.display = "block"; 
-    presentation.style.marginTop = window.innerHeight / 4 + "px";
-   presenta.style.position = "absolute" 
 }
 id = null ; 
-function presentami(){
-    
+function startshow(){
+    setTimeout(function(){
+        if(window.location.pathname.includes("index.html")){
+            window.location.pathname = window.location.pathname.replace("index.html","letter.html")
 
+        }else{
+            window.location.pathname = window.location.pathname + "letter.html"
+        }
+
+    },3000)
 }
---
+++ eli/main_letter.js
@@ -0,0 +1,10 @@
+
+
+
+var my_letter = document.getElementById("my_letter")
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = dd + '/' + mm + '/' + yyyy;
