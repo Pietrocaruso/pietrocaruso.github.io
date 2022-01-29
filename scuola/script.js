ee9c99236f662f958e94efe6807c06daec8095a8
Pietrocaruso <48248457+Pietrocaruso@users.noreply.github.com>
2022-01-29 23:01:17 +0100

    compleanno

---
+++ scuola/script.js
@@ -0,0 +1,20 @@
+function open_folder(name_folder){
+    if (window.location.pathname.includes("index.html")){
+        window.location.pathname = window.location.pathname.replace("/index.html","/"+ name_folder)
+    }else{
+        window.location.pathname = "/" +  name_folder
+    }
+}
+console.log(window.location.pathname)
+function back(){
+    if (window.location.pathname.includes("index.html")){
+
+    }else{
+        let test = "/ma/mari/ma/mae/"
+        for (i > test.length(); i++;){
+            
+        }
+    }
+}
+
+
