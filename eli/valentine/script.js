ce641fc7bc740ea52095c40fefb9707e9eb6928d
Pietrocaruso <48248457+Pietrocaruso@users.noreply.github.com>
2022-02-09 15:14:39 +0100

    some change

--- eli/valentine/script.js
+++ eli/valentine/script.js
@@ -1,54 +1,109 @@
-Splitting();
-const $ = document.querySelectorAll.bind(document);
-const chars = document.querySelectorAll('.char');
-const config = {
-  delay: 1,
-  repeat: 1,
-  yoyo: true,
-};
-const id = {};
-const elements = {
-  one: '.col1-row1',
-  two: '.col2-row1',
-  three: '.col3-row1',
-  four: '.col4-row1',
-  five: '.col1-row2',
-  six: '.col2-row2',
-  seven: '.col3-row2',
-  eight: '.col4-row2',
-  nine: '.col1-row3',
-  ten: '.col2-row3',
-  eleven: '.col3-row3',
-  twelve: '.col4-row3',
-  thirteen: '.col1-row4',
-  fourteen: '.col2-row4',
-  fifteen: '.col3-row4',
-  sixteen: '.col4-row4',
+/*
+  This pen cleverly utilizes SVG filters to create a "Morphing Text" effect. Essentially, it layers 2 text elements on top of each other, and blurs them depending on which text element should be more visible. Once the blurring is applied, both texts are fed through a threshold filter together, which produces the "gooey" effect. Check the CSS - Comment the #container rule's filter out to see how the blurring works!
+*/
+
+const elts = {
+  text1: document.getElementById("text1"),
+  text2: document.getElementById("text2")
 };
 
-Object.keys(elements).forEach(key => {
-  id[key] = $(`${elements[key]} .triangle`);
-});
+// The strings to morph between. You can change these to anything you want!
+const texts = [
+  "Ciao",
+  "sono",
+  "Pietro",
+  "Caruso",
+  "e",
+  "ti",
+  "auguro",
+  "un",
+  "buon",
+  "san",
+  "Valentino",
+  "❤",
+  " "
+];
 
-const tl = new TimelineMax(config);
+// Controls the speed of morphing.
+const morphTime = 0.75;
+const cooldownTime = 0.50;
 
-const explodeTime = 2;
-const explodeEase = SlowMo.ease.config(0.7, 0.7, false);
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
 
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
 
-tl
-  .to(id.one, explodeTime, { x: -500, y: -500, ease: explodeEase }, 'explode')
-  .to(id.two, explodeTime, { x: 400, y: -500, ease: explodeEase }, 'explode')
-  .to(id.three, explodeTime, { x: -400, y: -500, ease: explodeEase }, 'explode')
-  .to(id.four, explodeTime, { x: 500, y: -500, ease: explodeEase }, 'explode')
-  .to(id.five, explodeTime, { x: -500, y: -500, rotate: '-30deg', ease: explodeEase }, 'explode')
-  .to(id.six, explodeTime, { x: -500, y: 500, rotate: '30deg', ease: explodeEase }, 'explode')
-  .to(id.seven, explodeTime, { x: 400, y: -500, rotate: '-30deg', ease: explodeEase }, 'explode')
-  .to(id.eight, explodeTime, { x: 500, y: 500, rotate: '30deg', ease: explodeEase }, 'explode')
-  .to(id.nine, explodeTime, { x: -500, y: 400, ease: explodeEase }, 'explode')
-  .to(id.ten, explodeTime, { x: -600, y: 700, ease: explodeEase }, 'explode')
-  .to(id.eleven, explodeTime, { x: 600, y: 700, ease: explodeEase }, 'explode')
-  .to(id.twelve, explodeTime, { x: 500, y: 400, ease: explodeEase }, 'explode')
-  .to(id.fourteen, explodeTime, { x: -500, y: 500, ease: explodeEase }, 'explode')
-  .to(id.fifteen, explodeTime, { x: 500, y: 500, ease: explodeEase }, 'explode')
-  .staggerFrom(Array.from(chars), 1.5, { opacity:0, scale:0, y: 80, rotationX: 180, transformOrigin: "0% 50% -50",  ease: Back.easeOut }, 0.01, '-=1.2')
+function doMorph() {
+  morph -= cooldown;
+  cooldown = 0;
+
+  let fraction = morph / morphTime;
+
+  if (fraction > 1) {
+    cooldown = cooldownTime;
+    fraction = 1;
+  }
+
+  setMorph(fraction);
+}
+
+// A lot of the magic happens here, this is what applies the blur filter to the text.
+function setMorph(fraction) {
+  // fraction = Math.cos(fraction * Math.PI) / -2 + .5;
+
+  elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  fraction = 1 - fraction;
+  elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  elts.text1.textContent = texts[textIndex % texts.length];
+  elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+  morph = 0;
+
+  elts.text2.style.filter = "";
+  elts.text2.style.opacity = "100%";
+
+  elts.text1.style.filter = "";
+  elts.text1.style.opacity = "0%";
+}
+
+// Animation loop, which is called every frame.
+function animate() {
+  requestAnimationFrame(animate);
+
+  let newTime = new Date();
+  let shouldIncrementIndex = cooldown > 0;
+  let dt = (newTime - time) / 1000;
+  time = newTime;
+
+  cooldown -= dt;
+
+  if (cooldown <= 0) {
+    if (shouldIncrementIndex) {
+      textIndex++;
+    }
+
+    doMorph();
+  } else {
+    doCooldown();
+  }
+
+  if (elts.text1.textContent == "❤" && elts.text2.textContent == " "){
+    elts.text1.style.color = "##6b0911"
+    setTimeout(function redirect(){
+      window.location.pathname = window.location.pathname + "heart"
+    },500)
+  }
+
+}
+
+// Start the animation.
+animate();
